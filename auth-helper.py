7573ac8bc862e39a0e1b763c69a32c32ca3b8d5d
Travis Moore <travlmoore@gmail.com>
2024-05-13 15:12:16 -0500

    Create auth-helper.py

---
+++ auth-helper.py
@@ -0,0 +1,15 @@
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+requests.packages.urllib3.disable_warnings()
+url = "https://10.10.10.50/dna/system/api/v1/auth/token"
+
+response = requests.request(
+    "POST",
+    url,
+    auth=HTTPBasicAuth("admin", "1234QWer"),
+    verify=False,
+)
+
+print(response.json())
